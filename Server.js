6d7de1e109778160649cf3440c48ff02be61aa78
Alexander Ferrard <aferrard@purdue.edu>
2018-09-10 21:07:23 -0400

    New Branch

---
+++ Server.js
@@ -0,0 +1,24 @@
+//load necessities
+var express = require('express');
+
+var app = express();
+var bodyParser = require('body-parser');
+//var mysql = require('mysql');
+//var cookieParser = require('cookie-parser');
+var path = require('path');
+
+//app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '')));
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+//user res.render to load up an ejs view file
+
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+app.listen(6009);
+console.log('6009 is the open port');
