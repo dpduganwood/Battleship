5239adc9fb1c341dc6cf84c5bcc807708e648ad3
Alexander Ferrard <aferrard@purdue.edu>
2018-09-12 14:38:07 -0400

    SQL connection

--- Server.js
+++ Server.js
@@ -12,6 +12,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '')));
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "sql123",
+    database: "BattletracksDB"
+});
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected with Database");
+});
+
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 //user res.render to load up an ejs view file
