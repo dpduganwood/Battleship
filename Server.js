747a92d84eaefac2d5e700cd63b7a50c98e48ec7
Alexander Ferrard <aferrard@purdue.edu>
2018-09-15 16:30:23 -0400

    index and server fix

--- Server.js
+++ Server.js
@@ -21,6 +21,7 @@ var con = mysql.createConnection({
     password: "sql123",
     database: "Battletracks_db"
 });
+
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected with Database");
@@ -32,6 +33,68 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
+
+/*app.get('/register', function (req, res) {
+    res.render('pages/register');
+});*/
+
+/*app.post('/register', function (req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    if (email.length == 0) {
+        //userloggedincheck(req, function (loggedin) {
+            res.render('pages/registration', {
+                //loggedin: loggedin,
+                //username: req.cookies.user,
+                perror: "Please enter valid email"
+            })
+        //});
+        return;
+    } else if (username.length == 0) {
+        //userloggedincheck(req, function (loggedin) {
+            res.render('pages/registration', {
+                //loggedin: loggedin,
+                //username: req.cookies.user,
+                perror: "Username Cannot be empty"
+            })
+        //});
+    } else if (password.length == 0) {
+        //userloggedincheck(req, function (loggedin) {
+            res.render('pages/registration', {
+                //loggedin: loggedin,
+                //username: req.cookies.user,
+                perror: "Password Cannot be empty"
+            })
+        //});
+    }
+    else {
+        Connection.getUserByUsername(username, function (result) {
+            //console.log(result);
+            if (result != "user does not exist") {
+                //userloggedincheck(req, function (loggedin) {
+                    res.render('pages/registration', {
+                        //loggedin: loggedin,
+                        //username: req.cookies.user,
+                        perror: "Username already exists, Please choose a different Username"
+                    })
+                //});
+            } else {
+                var Person = new User(username, password, email);
+                Connection.registerUser(email, username, password, function (result) {
+                    console.log(result);
+                    res.render('pages/registration', {
+                        perror: "Successfully Registered"
+                    });
+                    map.delete(username);
+                    return;
+                });
+            }
+        });
+    }
+    return;
+});
+*/
 
 app.listen(6009);
 console.log('6009 is the open port');
