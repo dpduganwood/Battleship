efca96b48f0cfd1a380c7c750deacce5c1907f60
David Wood <wood154@purdue.edu>
2018-09-21 20:24:28 -0400

    addPlayerHit unit test

--- spec/ConnectionSpec.js
+++ spec/ConnectionSpec.js
@@ -1,27 +1,37 @@
 var Connection = require("../Connection.js");
 
+var prof = {
+    displayName: "David Wood",
+    emails: [{value: "dpduganwood@gmail.com"}],
+    name: {givenName: "David", familyName: "Wood"}
+};
+
+var disp = {
+    displayName: "David Wood"
+}
+
 describe("checkHit_test",function(){
     it("Testing login",function() {
-        Connection.con.query("REMOVE FROM users WHERE email = 'dpduganwood@gmail.com'", function (err, result) {
-            if (err) {
-                //test failure
-            } else {
-                //login
-                var prof = {
-                    displayName: "David Wood",
-                    emails: [{value: "dpduganwood@gmail.com"}],
-                    name: {givenName: "David", familyName: "Wood"}
-                };
-                Connection.procUser(prof, function (result) {
-                    Connection.con.query("SELECT * FROM users WHERE displayName = '" + prof.displayName + "'", function (err, result) {
-                        if (err) {
-                            //failure
-                        } else {
-                            expect(result.length).toBe(1);
-                        }
-                    });
+        Connection.con.query("REMOVE FROM users WHERE email = 'dpduganwood@gmail.com'", function () {
+            Connection.procUser(prof, function (result) {
+                Connection.con.query("SELECT * FROM users WHERE displayName = '" + prof.displayName + "'", function (err, result) {
+                    expect(result.length).toBe(1);
                 });
-            }
+            });
+
+        });
+    });
+});
+
+describe("addPlayerHit",function(){
+    it("Testing add player hit function",function() {
+        Connection.con.query("REMOVE FROM users WHERE email = 'dpduganwood@gmail.com'", function () {
+            Connection.addPlayerHit(function (result) {
+                Connection.con.query("SELECT * FROM users WHERE displayName = '" + disp.displayName + "'", function (err, result) {
+                    expect(result.length).toBe(1);
+                });
+            });
+
         });
     });
 });
